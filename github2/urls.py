b3ecb73922a6400b6d268f2c26bc20dcdb0c56c9
Jiyeon981225 <wldus01225@gmail.com>
2019-04-29 12:41:07 +0000

    why

--- github2/urls.py
+++ github2/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name="home"),
+    url(r'^new/$', views.new, name="new"),
+    url(r'^post/(?P<index>d+)/$', views.post_detail, name = 'post_detail'),
+    url(r'^post/(?P<index>\d+)/edit/$', views.edit, name='edit'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.delete, name='delete'),
 ]
